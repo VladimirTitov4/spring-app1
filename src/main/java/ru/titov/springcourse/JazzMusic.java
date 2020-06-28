4dfd8d5b47a91bb9d56563c33bdd4d9d8f47fc34
VladimirTitov4 <vtitov4@yandex.ru>
2020-06-28 11:17:26 +0400

    Injected dependency as a List

---
+++ src/main/java/ru/titov/springcourse/JazzMusic.java
@@ -0,0 +1,8 @@
+package ru.titov.springcourse;
+
+public class JazzMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Some jazz song";
+    }
+}
---
+++ src/main/java/ru/titov/springcourse/PopMusic.java
@@ -0,0 +1,8 @@
+package ru.titov.springcourse;
+
+public class PopMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Some pop music";
+    }
+}
